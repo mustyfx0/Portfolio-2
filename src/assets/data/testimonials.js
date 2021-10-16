9529766ffbbc76e579adc7dc7c9f825b726b686b
Mustapha Yusuf <85568073+mustyfx0@users.noreply.github.com>
2021-10-16 20:26:51 +0300

    Update testimonials.js

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -2,26 +2,22 @@ const testimonials = [
   {
     id: 1,
     name: 'Shaif Arfan',
-    title: 'CEO',
-    org: 'WEB CIFAR',
     desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+      'Mustapha is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
   },
   {
     id: 2,
     name: 'Fahim Elahe',
-    title: 'Co-founder',
-    org: 'Kreetive',
     desc:
       'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
   },
   {
     id: 3,
-    name: 'Sayed',
+    name: 'Gideon',
     title: 'Marketing Head',
-    org: 'Eleven Dawn',
+    org: 'Jibasa Tech',
     desc:
-      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+      'Really hard-working person. Deliver the work as promised. Planning to work more with him.',
   },
 ];
 export default testimonials;
